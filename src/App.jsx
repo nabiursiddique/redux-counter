bbbf0eb231b3cafa7a818df284a77c36c603816e
nabiursiddique <nabiursiddique01@gmail.com>
2023-08-09 21:48:03 +0600

    Design: Basic design is completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-function App() {
+import Counter from "./components/Counter/Counter"
 
+const App = () => {
   return (
-    <div>
-      <h1>Redux Counter</h1>
+    <div className="bg-gradient-to-r from-indigo-500">
+      <div className="h-screen flex items-center justify-center">
+        <div>
+          <h1 className="text-center text-3xl text-white font-bold">Redux Counters</h1>
+          <Counter />
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,7 +1,14 @@
+import Button from "../Button/Button";
+
 const Counter = () => {
     return (
-        <div>
-
+        <div className="shadow-xl p-10 my-5 bg-white rounded-lg">
+            <h2 className="text-6xl text-center text-gray-600 my-10 font-bold">0</h2>
+            <div>
+                <Button>Increment</Button>
+                <Button>Decrement</Button>
+                <Button>Reset</Button>
+            </div>
         </div>
     );
 };
