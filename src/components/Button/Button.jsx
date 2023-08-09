a0c4fe835aed4c7f27a6011f7ef0e3ebd8340279
nabiursiddique <nabiursiddique01@gmail.com>
2023-08-09 22:17:39 +0600

    feature: Basic counter with redux is completed

--- src/components/Button/Button.jsx
+++
@@ -1,7 +0,0 @@
-const Button = ({ children }) => {
-    return (
-        <button className="text-xl bg-blue-400 mx-5 px-5 py-3 text-white rounded hover:bg-blue-600 shadow-lg">{children}</button>
-    );
-};
-
-export default Button;
--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,13 +1,30 @@
-import Button from "../Button/Button";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment, reset } from "../../redux/counter/ActionCreators";
 
 const Counter = () => {
+    const count = useSelector((state) => state.count);
+    const dispatch = useDispatch();
+
+    const handleIncrement = () => {
+        dispatch(increment());
+    };
+
+    const handleDecrement = () => {
+        dispatch(decrement());
+    };
+
+    const handleReset = () => {
+        dispatch(reset());
+    };
+
     return (
         <div className="shadow-xl p-10 my-5 bg-white rounded-lg">
-            <h2 className="text-6xl text-center text-gray-600 my-10 font-bold">0</h2>
+            <h2 className="text-6xl text-center text-gray-600 my-10 font-bold">{count}</h2>
             <div>
-                <Button>Increment</Button>
-                <Button>Decrement</Button>
-                <Button>Reset</Button>
+                <button onClick={handleIncrement} className="text-xl bg-blue-400 mx-5 px-5 py-3 text-white rounded hover:bg-blue-600 shadow-lg">Increment</button>
+                <button onClick={handleDecrement} className="text-xl bg-blue-400 mx-5 px-5 py-3 text-white rounded hover:bg-blue-600 shadow-lg">Decrement</button>
+                <button onClick={handleReset} className="text-xl bg-blue-400 mx-5 px-5 py-3 text-white rounded hover:bg-blue-600 shadow-lg">Reset</button>
+
             </div>
         </div>
     );
