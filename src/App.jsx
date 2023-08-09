ddbc2f918b92489c4e99372195e36e985309ce2f
nabiursiddique <nabiursiddique01@gmail.com>
2023-08-10 01:20:33 +0600

    fix: little design fixes

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,14 @@ import Counter from "./components/Counter/Counter"
 const App = () => {
   return (
     <div className="bg-gradient-to-r from-indigo-500">
-      <div className="h-screen flex items-center justify-center">
+      <div className="h-full flex items-center justify-center py-10">
         <div>
-          <h1 className="text-center text-3xl text-white font-bold">Redux Counters</h1>
+          <h1 className="text-center text-4xl text-white font-bold mb-5 ">Redux Counters</h1>
+          {/* Basic Counter  */}
+          <h3 className="text-center text-xl text-white font-bold">Basic Counter</h3>
+          <Counter />
+          {/* Dynamic Counter  */}
+          <h3 className="text-center text-xl text-white font-bold mt-16">Dynamic Counter</h3>
           <Counter />
         </div>
       </div>
--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -18,12 +18,12 @@ const Counter = () => {
     };
 
     return (
-        <div className="shadow-xl p-10 my-5 bg-white rounded-lg">
+        <div className="shadow-xl p-10 my-5 bg-white rounded-lg px-3">
             <h2 className="text-6xl text-center text-gray-600 my-10 font-bold">{count}</h2>
-            <div>
-                <button onClick={handleIncrement} className="text-xl bg-blue-400 mx-5 px-5 py-3 text-white rounded hover:bg-blue-600 shadow-lg">Increment</button>
-                <button onClick={handleDecrement} className="text-xl bg-blue-400 mx-5 px-5 py-3 text-white rounded hover:bg-blue-600 shadow-lg">Decrement</button>
-                <button onClick={handleReset} className="text-xl bg-blue-400 mx-5 px-5 py-3 text-white rounded hover:bg-blue-600 shadow-lg">Reset</button>
+            <div className="grid lg:grid-cols-3 grid-cols-2">
+                <button onClick={handleIncrement} className="text-xl bg-blue-400 mx-5 my-3 px-5 py-3 text-white rounded hover:bg-blue-600 shadow-lg">Increment</button>
+                <button onClick={handleDecrement} className="text-xl bg-blue-400 mx-5 my-3 px-5 py-3 text-white rounded hover:bg-blue-600 shadow-lg">Decrement</button>
+                <button onClick={handleReset} className="text-xl bg-blue-400 mx-5 my-3 px-5 py-3 text-white rounded hover:bg-blue-600 shadow-lg">Reset</button>
 
             </div>
         </div>
